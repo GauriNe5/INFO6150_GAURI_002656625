88ad8ddfb9ba209e83f70816c20cbe1c9c7da355
GauriNe5 <103602989+GauriNe5@users.noreply.github.com>
2022-11-27 12:07:47 -0800

    Delete Project9_React_portfolio directory

--- Project9_React_portfolio/README.md
+++
@@ -1,2 +0,0 @@
-
-Personal React portfolio 
--- Project9_React_portfolio/src/components/PageTransition.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const PageTransition = styled.div`
-  animation-name: moveFromTop;
-  animation-duration: 0.7s;
-  animation-timing-function: ease-in-out;
-`;
-
-export default PageTransition;
